a1cd98ccc868820384b2931948b7c1c7ab079ae2
Mark Hanna <59896894+maha4028@users.noreply.github.com>
2025-11-06 11:47:45 -0700

    Update 1_select_statement.sql

--- 1_select_statement.sql
+++ 1_select_statement.sql
@@ -1,13 +1,13 @@
 SELECT *
-FROM Parks_and_Recreation.employee_demographics;
+FROM pandr.employee_demographics;
 
 SELECT first_name, 
 last_name,
 birth_date,
 age,
 (age + 10)*10 + 10
-FROM Parks_and_Recreation.employee_demographics;
+FROM pandr.employee_demographics;
 # PEMDAS
 
 SELECT DISTINCT first_name, gender
-FROM Parks_and_Recreation.employee_demographics;
+FROM pandr.employee_demographics;
